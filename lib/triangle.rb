2711bd3d55b9cb22a47f725ded1d5f82a65e42d4
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-05 22:26:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,17 @@ class Triangle
   end
 
   def kind
-     if @a == @b && @b == @c
+
+    if a <= 0 || b <= 0 || c <= 0
+      begin
+      raise TriangleError
+      end
+    elsif (a + b) <= c || (a + c) <= b || (b + c) <= a
+      begin
+      raise TriangleError
+    elsif a == b && b == c
        :equilateral
-     elsif @a == @b || @b == @c|| @c == @a
+     elsif a == b || b == c|| c == a
        :isosceles
      else
        :scalene
@@ -19,15 +27,7 @@ class Triangle
   end
 
   def valid
-    if a <= 0 || b <= 0 || c <= 0
-      begin
-      raise TriangleError
-      end
-    else (a + b) <= c || (a + c) <= b || (b + c) <= a
-      begin
-      raise TriangleError
-    end
-    end
+    
   end
 
   class TriangleError < StandardError
