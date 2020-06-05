798781e2c86adf0b4027f66558d7d7eddbf605b1
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-05 22:17:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+     if @a == @b && @b == @c
+       :equilateral
+     elsif @a == @b || @b == @c|| @c == @a
+       :isosceles
+     else
+       :scalene
+     end
+  end
+
+  def valid?
+    if @a <= 0 || @b <= 0 || @c <= 0
+      raise TriangleError
+    elsif (@a + @b) <= @c || (@a + @c) <= @b || (@b + @c) <= @a
+      raise TriangleError
+    else
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
