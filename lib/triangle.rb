33688756b8041dc5ffea9a6544a79ff4ce0ceb06
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-05 22:28:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,17 +10,14 @@ class Triangle
 
   def kind
 
-    if a <= 0 || b <= 0 || c <= 0
+    if a <= 0 || b <= 0 || c <= 0 || (a + b) <= c || (a + c) <= b || (b + c) <= a
       begin
       raise TriangleError
       end
-    elsif (a + b) <= c || (a + c) <= b || (b + c) <= a
-      begin
-      raise TriangleError
     elsif a == b && b == c
-       :equilateral
+       self.kind = :equilateral
      elsif a == b || b == c|| c == a
-       :isosceles
+       self.kind = :isosceles
      else
        :scalene
      end
